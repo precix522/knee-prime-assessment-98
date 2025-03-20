541aa0de738a044a7b6a529b751ef2dc1817f17e
ragavi522 <ragavi.nandhakumar@precix.io>
2025-03-21 00:30:36 +0530

    Update PrivacyPolicy.tsx

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -70,9 +70,8 @@ const PrivacyPolicy = () => {
               </section>
               <section>
                 <h2 className="text-2xl font-semibold text-gray-900 mb-4">Contact Us</h2>
-                <p>
-                  If you have questions or requests, please feel free to contact us at cs@precix.io 
-                </p>
+                <p className="mb-4">
+                  If you have questions or requests, please feel free to contact us at cs@precix.io </p>
                 <p className="mb-4">Because email communications are not always secure, please do not include sensitive information in your emails to us.</p>
               </section>
             </div>
