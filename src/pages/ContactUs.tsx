36851ba607b95c45e4ebc45a5e83cefb80710ef9
ragavi522 <ragavi.nandhakumar@precix.io>
2025-03-20 13:19:21 +0800

    Update ContactUs.tsx

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -122,7 +122,7 @@ const ContactUs = () => {
                   </div>
                   <div>
                     <h3 className="text-lg font-semibold text-gray-900">Phone</h3>
-                    <p className="text-gray-600"> 882 780-00</p>
+                    <p className="text-gray-600"> +65 88278000</p>
                     <p className="text-gray-500 text-sm mt-1">Monday-Friday, 9AM-5PM EST</p>
                   </div>
                 </div>
