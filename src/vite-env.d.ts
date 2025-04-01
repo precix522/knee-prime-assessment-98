90ed0e9d9a2678aa938a5205456c3547f0c93286
ragavi522 <ragavi.nandhakumar@precix.io>
2025-04-01 19:27:22 +0530

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,9 +2,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_TWILIO_ACCOUNT_SID: string;
-  readonly VITE_TWILIO_AUTH_TOKEN: string;
-  readonly VITE_TWILIO_SERVICE_SID: string;
+  readonly VITE_TWILIO_ACCOUNT_SID: 'ACcbbc17f108e418e864e4f76f20fbef73';
+  readonly VITE_TWILIO_AUTH_TOKEN: '6942e316dc6ced823475d26512178cac';
+  readonly VITE_TWILIO_SERVICE_SID: 'VA3c04abf5a6865c4706ef361f59f7215c';
 }
 
 interface ImportMeta {
