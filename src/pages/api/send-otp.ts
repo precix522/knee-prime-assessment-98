cc3bf917c83ff47eb193f07960df403dc8a78372
precix <ragavi.nandhakumar@precix.io>
2025-05-09 09:13:14 +0530

    Update send-otp.ts

--- src/pages/api/send-otp.ts
+++ src/pages/api/send-otp.ts
@@ -4,7 +4,7 @@ import { sendOTP as twilioSendOTP } from '../../api/twilio-service';
 import { sendOTP as vonageSendOTP } from '../../api/vonage-service';
 
 // Default to using Vonage service
-const OTP_SERVICE: 'twilio' | 'vonage' = 'vonage';
+const OTP_SERVICE: 'twilio';
 
 export default async function handleSendOTP(request: Request): Promise<Response> {
   try {
